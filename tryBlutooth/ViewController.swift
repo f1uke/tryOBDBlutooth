56a3654a55903e6694dcb769228af8683f90bfa3
Fluke <lingfluke@gmail.com>
2021-03-05 22:29:59 +0700

    background location

--- tryBlutooth/ViewController.swift
+++ tryBlutooth/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 import CoreBluetooth
+import Alamofire
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var okBtn: UIButton!
+    
+    private var locations: [MKPointAnnotation] = []
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.allowsBackgroundLocationUpdates = true
+        return manager
+    }()
+    
+    var updateTimer: Timer?
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
     
     let obdServiceCBUUID = CBUUID(string: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2")
     let obdCharacteristicCBUUID = CBUUID(string: "BEF8D6C9-9C21-4C9E-B632-BD58C1009F9F")
@@ -26,14 +42,20 @@ class ViewController: UIViewController {
         
         obdBluetoothIO = ObdBluetoothIO(serviceUUID: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2", obdCharacteristic: "BEF8D6C9-9C21-4C9E-B632-BD58C1009F9F", deviceName: "IOS-Vlink",
                                         onConnected: {
-//                                            self.sendInitCommand()
+                                            self.sendInitCommand()
                                         })
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(reinstateBackgroundTask), name: UIApplication.didBecomeActiveNotification, object: nil)
         
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     @IBAction func okPressed(_ sender: Any) {
-        sendInitCommand()
-        
+//        sendInitCommand()
+        locationManager.startUpdatingLocation()
     }
     @IBAction func Pressed0100(_ sender: Any) {
         obdBluetoothIO.writeValue(value: "0100\r\n") { (resp) in
@@ -69,5 +91,74 @@ class ViewController: UIViewController {
                                                 print("ViewController Resp = \(resp)")
                                                 self.obdBluetoothIO.writeValue(value: "ATH0\r\n") { (resp) in
                                                     print("ViewController Resp = \(resp)")
+                                                    
+                                                    self.updateTimer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                                                                            selector: #selector(self.call101C), userInfo: nil, repeats: true)
+                                                    // register background task
+                                                    self.registerBackgroundTask()
+                                                    
                                                 }}}}}}}}}}}}
+    
+    @objc func call101C() {
+        obdBluetoothIO.writeValue(value: "010C\r\n") { (resp) in
+            print("ViewController Resp = \(resp)")
+        }
+        
+        AF.request("https://httpbin.org/get").response { response in
+            print(response)
+        }
+    }
+    
+    @objc func reinstateBackgroundTask() {
+        if updateTimer != nil && backgroundTask == .invalid {
+            registerBackgroundTask()
+        }
+    }
+    
+    func registerBackgroundTask() {
+        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+            self?.endBackgroundTask()
+        }
+        assert(backgroundTask != .invalid)
+    }
+    
+    func endBackgroundTask() {
+        print("Background task ended.")
+        UIApplication.shared.endBackgroundTask(backgroundTask)
+        backgroundTask = .invalid
+    }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let mostRecentLocation = locations.last else {
+            return
+        }
+        
+        // Add another annotation to the map.
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = mostRecentLocation.coordinate
+        
+        // Also add to our map so we can remove old values later
+        self.locations.append(annotation)
+        
+        // Remove values if the array is too big
+//        while locations.count > 100 {
+            let annotationToRemove = self.locations.first!
+            self.locations.remove(at: 0)
+            
+            // Also remove from the map
+//            mapView.removeAnnotation(annotationToRemove)
+//        }
+        
+        if UIApplication.shared.applicationState == .active {
+//            mapView.showAnnotations(self.locations, animated: true)
+            print("New location is %@", mostRecentLocation)
+        } else {
+            print("App is backgrounded. New location is %@", mostRecentLocation)
+        }
+    }
+    
 }
